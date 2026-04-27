0af75c993ab48bfddb510dc46829a76fddb135bb
red-shinku <3480198982@qq.com>
2026-04-27 20:02:27 +0800

    fix: Course表只记录课程，通过新建CourseOffering表独立班级选课，并修改对应的外键。调整建表顺序

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,4 +1,4 @@
-create database if not exits LabSystem
+create database if not exists LabSystem
     DEFAULT CHARACTER SET utf8mb4
     DEFAULT COLLATE utf8mb4_general_ci;
 
@@ -9,45 +9,13 @@ create table Users(
 --     用户标识
     UUID int unsigned AUTO_INCREMENT PRIMARY KEY,
 --     身份
-    identity tinyint unsigned,
+    identity tinyint unsigned not null comment '1=学生, 2=教师, 9=管理员',
 --     学号/工号
     number varchar(16) unique not null,
 --     邮箱
-    mail varchar(24) unique not null,
+    mail varchar(32) unique not null,
 --     密码，加密存储
-    passwd varchar(24)
-);
-
--- 实验项目表
-create table Project(
---     项目标识
-    projectID int unsigned AUTO_INCREMENT PRIMARY KEY,
---     项目名
-    projectName varchar(24),
---     所属课程ID
-    courseID FOREIGN KEY references Course(courseID),
---     项目文件路径
-    projectFilePath varchar(24),
---     开始时间
-    startTime time,
---     截止时间
-    deadline time,
--- 项目开启状态
-    isActive bool
-);
-
--- 学生实验报告表
-create table StuReport(
---     实验报告标识
-    stuReportID int unsigned AUTO_INCREMENT PRIMARY KEY,
---     课程标识
-    courseID FOREIGN KEY references Course(courseID),
---     项目标识
-    projectID FOREIGN KEY references Project(projectID),
---     报告文件路径
-    reportFilePath varchar(24),
---     提交时间
-    submitTime time,
+    passwd varchar(255) not null
 );
 
 -- 课程表
@@ -55,21 +23,74 @@ create table Course(
 --     课程标识
     courseID int unsigned AUTO_INCREMENT PRIMARY KEY,
 --     课程名
-    courseName varchar(24),
+    courseName varchar(24)
 );
 
--- 学生信息表
-create table Student(
+-- 选课表，课程与班级
+CREATE TABLE CourseOffering (
+    offeringID int unsigned AUTO_INCREMENT PRIMARY KEY,
+    courseID int unsigned not null,
+--     选课班级
+    className varchar(32) not null,
+--     学期
+    term varchar(32) not null,
+
+    FOREIGN KEY (courseID) REFERENCES Course(courseID) ON DELETE CASCAD
+);
+
+-- 学生信息表（所选课程）
+create table StudentCourse(
 --     学号
-    stuID int unsigned,
+    studentID int unsigned,
 --     参加的课程
-    courseID FOREIGN KEY references Course(courseID)
+    offeringID int unsigned not null,
+
+    FOREIGN KEY (offeringID) REFERENCES Course(offeringID) ON DELETE CASCAD
 );
 
--- 教师信息表
-create table Teacher(
+-- 教师信息表（所管课程）
+create table TeacherCourse(
 --     工号
-    stuID int unsigned,
---     参加的课程
-    courseID FOREIGN KEY references Course(courseID)
+    teacherID int unsigned,
+--     管理的课程
+    offeringID int unsigned not null,
+
+    FOREIGN KEY (offeringID) REFERENCES Course(offeringID) ON DELETE CASCAD
 );
+
+-- 实验项目表
+create table Project(
+--     项目标识
+    projectID int unsigned AUTO_INCREMENT PRIMARY KEY,
+--     所属课程ID，对应选课表的主键，允许同一个课程的不同班级拥有不同项目
+    offeringID int unsigned not null,
+--     项目名
+    projectName varchar(64),
+--     项目文件路径
+    projectFilePath varchar(128),
+--     开始时间
+    startTime datetime,
+--     截止时间
+    deadline datetime,
+-- 项目开启状态
+    isActive bool not null default false,
+
+    FOREIGN KEY (offeringID) REFERENCES CourseOffering(offeringID) ON DELETE CASCADE
+);
+
+-- 学生实验报告表
+create table StuReport(
+--     实验报告标识
+    stuReportID int unsigned AUTO_INCREMENT PRIMARY KEY,
+--     学生ID
+    studentID int unsigned not null,
+--     项目标识
+    projectID int unsigned not null,
+--     报告文件路径
+    reportFilePath varchar(128),
+--     提交时间
+    submitTime datetime,
+    FOREIGN KEY (studentID) REFERENCES StudentCourse(studentID) ON DELETE CASCADE,
+    FOREIGN KEY (projectID) REFERENCES Project(projectID) ON DELETE CASCADE
+);
+
