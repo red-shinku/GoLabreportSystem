f727e11df0299cad140b0e1a4b7360bb701d5cd4
red-shinku <3480198982@qq.com>
2026-05-02 11:59:32 +0800

    add: 文件业务元数据，安全路径生成、文件类型检查

--- domain/serviceData.go
+++ domain/serviceData.go
@@ -2,10 +2,23 @@ package domain
 
 import (
 	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 )
 
 var ErrAuth = errors.New("failed to auth")
+
+var (
+	ErrNotSafe  = errors.New("not safe")
+	ErrNotAllow = errors.New("not allow")
+)
+
+const baseDir string = "./files"
+
+var fileFmtSet map[string]struct{}
 
 type LoginUserInfo struct {
 	Identity uint8
@@ -36,4 +49,51 @@ type ProjectItem struct {
 	ProjectID   uint
 	// 用于学生端显示提交状态
 	SubmitStatus bool
+}
+
+// StuReportMeta 学生报告业务元数据，包含所属课程等信息
+type StuReportMeta struct {
+	courseName  string
+	className   string
+	studentID   string
+	studentName string
+	projectName string
+	format      string
+}
+
+// FilePath 返回安全的文件路径
+func (srm *StuReportMeta) FilePath() (string, error) {
+	filename := fmt.Sprintf("%s-%s-%s.%s", srm.studentID, srm.studentName, srm.projectName, srm.format)
+	path := filepath.Join(baseDir, srm.courseName, srm.className, srm.projectName, filename)
+	cleanPath := filepath.Clean(path)
+	if !strings.HasPrefix(cleanPath, baseDir+string(os.PathSeparator)) {
+		return "", fmt.Errorf("StuReportMeta(): path %w", ErrNotSafe)
+	}
+	return cleanPath, nil
+}
+
+// Check 检查文件格式
+func (srm *StuReportMeta) Check() error {
+	if _, ok := fileFmtSet[srm.format]; !ok {
+		return ErrNotAllow
+	}
+	return nil
+}
+
+// ProjectFileMeta 项目要求文件的业务元数据
+type ProjectFileMeta struct {
+	courseName  string
+	className   string
+	projectName string
+	fileName    string
+}
+
+// FilePath 返回安全的文件路径
+func (pfm *ProjectFileMeta) FilePath() (string, error) {
+	path := filepath.Join(baseDir, pfm.courseName, pfm.className, pfm.projectName, pfm.fileName)
+	cleanPath := filepath.Clean(path)
+	if !strings.HasPrefix(cleanPath, baseDir+string(os.PathSeparator)) {
+		return "", fmt.Errorf("StuReportMeta(): path not save")
+	}
+	return cleanPath, nil
 }
