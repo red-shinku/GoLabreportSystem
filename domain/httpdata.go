f0824afe3e67273e2a33c1ddd9892bfaf7b2920c
red-shinku <3480198982@qq.com>
2026-05-06 10:22:12 +0800

    fix: 移除Form类型多余的字段。这种类型不只限于表单数据，而接受控制层解析HTTP得到的所有数据。添加ProjectFileForm，专用于教师重传项目文件。

--- domain/httpdata.go
+++ domain/httpdata.go
@@ -2,40 +2,52 @@ package domain
 
 import "time"
 
-// ProjectForm 当上传项目要求文件时，从表单获取的相关数据
+// ProjectForm 当新建项目时，从表单或请求中获取的相关数据
 // 填充完毕后会转交给业务层，用于下一步的DTO构建
 type ProjectForm struct {
 	OfferingID  uint
-	ProjectID   uint
 	ProjectName string
 	FileName    string
 	CloseTime   time.Time
 }
 
 // NewProjectForm 创建ProjectForm实例
-func NewProjectForm(offeringID, projectID uint, projectName, fileName string, closeTime time.Time) *ProjectForm {
+func NewProjectForm(offeringID uint, projectName, fileName string, closeTime time.Time) *ProjectForm {
 	return &ProjectForm{
 		OfferingID:  offeringID,
-		ProjectID:   projectID,
 		ProjectName: projectName,
 		FileName:    fileName,
 		CloseTime:   closeTime,
 	}
 }
 
+// ProjectFileForm 当重传项目要求文件时，从表单或请求中获取的数据
+type ProjectFileForm struct {
+	ProjectID uint
+	FileName  string
+}
+
+func NewProjectFileForm(projectID uint, fileName string) *ProjectFileForm {
+	return &ProjectFileForm{
+		ProjectID: projectID,
+		FileName:  fileName,
+	}
+}
+
 type StuReportForm struct {
-	StudentID  string
-	ProjectID  uint
-	Format     string
-	SubmitTime time.Time
+	StudentID string
+	ProjectID uint
+	Format    string
 }
 
 // NewStuReportForm 创建StuReportForm实例
-func NewStuReportForm(studentID string, projectID uint, format string, submitTime time.Time) *StuReportForm {
+func NewStuReportForm(studentID string, projectID uint, format string) *StuReportForm {
 	return &StuReportForm{
-		StudentID:  studentID,
-		ProjectID:  projectID,
-		Format:     format,
-		SubmitTime: submitTime,
+		// 控制层获取token中的学生ID
+		StudentID: studentID,
+		// 控制层从URL获取项目ID
+		ProjectID: projectID,
+		// 控制层获取 Content-type并填充
+		Format: format,
 	}
 }
