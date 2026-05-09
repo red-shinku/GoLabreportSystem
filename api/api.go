9331aeb59bb94c3707eb95c981fc32171c3fe23b
red-shinku <3480198982@qq.com>
2026-05-09 11:19:07 +0800

    add: 定义JWT结构，用于使用接口的资格验证、以及身份辨识。包含用户号码与身份。

--- api/api.go
+++ api/api.go
@@ -1,8 +1,20 @@
 package api
 
-import "time"
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
 
 // 存放请求/响应的DTO
+
+// LoginJWT 自定义JWT的结构
+// 该JWT在通过登录时发放
+// 除登录与忘记密码，其他接口均需要验证该JWT
+type LoginJWT struct {
+	UserID string `json:"user_id"`
+	Role   string `json:"identity"`
+	jwt.RegisteredClaims
+}
 
 // LoginRequest 登录POST方法的请求体
 type LoginRequest struct {
@@ -31,27 +43,27 @@ type ReportFileFormRequest struct {
 	Filename string `json:"filename"`
 }
 
-// identityMap 身份码到身份的映射表，用于设置cookie
-var identityMap = map[uint8]string{
+// roleMap 身份码到身份的映射表，用于设置cookie
+var roleMap = map[uint8]string{
 	1: "student",
 	2: "teacher",
 	3: "operator",
 }
 
-// Identity 根据身份码获取身份字符串
-func Identity(code uint8) (string, bool) {
-	identity, ok := identityMap[code]
-	return identity, ok
+// Role 根据身份码获取身份字符串
+func Role(code uint8) (string, bool) {
+	role, ok := roleMap[code]
+	return role, ok
 }
 
-var identityCodeMap = map[string]uint8{
+var identityMap = map[string]uint8{
 	"student":  1,
 	"teacher":  2,
 	"operator": 3,
 }
 
-// IdentityCode 根据身份字符串获取身份码
-func IdentityCode(identity string) (uint8, bool) {
-	code, ok := identityCodeMap[identity]
+// Identity 根据身份字符串获取身份码
+func Identity(role string) (uint8, bool) {
+	code, ok := identityMap[role]
 	return code, ok
 }
